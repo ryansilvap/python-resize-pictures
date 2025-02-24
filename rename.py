506e41de63ed848958e07a7d09b421e0019126eb
Ryan Pereira <ryan.pereira.s10@gmail.com>
2025-02-24 20:30:25 -0300

    Inserindo o script de compactação junto ao script de renomear os arquivos

--- rename.py
+++ rename.py
@@ -1,4 +1,5 @@
-import os
+import os, shutil, PIL
+from PIL import Image
 
 folder = r'C:\Users\ryan.pereira\Desktop\Imagens'
 
@@ -13,9 +14,20 @@ for path, directories, files in os.walk(folder):
 
   for file in files:
     full_name, extension = os.path.splitext(file)
+    new_name = f'{full_name} (Large){extension}'
     if extension.lower() == '.jpg':
       if int(os.path.getsize(f'{path}/{file}')) > 700000 and file != 'Thumbs.db':
 
+        #resizing images
+        image_width = 1440
+        image_height = 1080
+
+        with Image.open(os.path.join(path, file)) as image:
+          image = image.resize((image_width, image_height), PIL.Image.NEAREST)
+          image.save(os.path.join(path, new_name))
+          shutil.copystat(os.path.join(path, file), os.path.join(path, new_name))
+
+        #renaming files
         if len(full_name.split(' ')) == 2:
           first_name, second_name = full_name.split(' ')
           first_name = first_name.strip()
