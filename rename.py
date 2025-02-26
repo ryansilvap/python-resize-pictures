eb7b2365b3d1e5ac97624647fe6d976400805f27
Ryan Pereira <ryan.pereira.s10@gmail.com>
2025-02-26 18:31:39 -0300

    Adicionando a funcionalidade de remoção dos arquivos com (Large) no nome no script principal

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-import os, shutil, PIL
+import os, re, shutil, PIL
 from PIL import Image
 
 folder = r'C:\Users\ryan.pereira\Desktop\Imagens'
@@ -13,23 +13,29 @@ for path, directories, files in os.walk(folder):
   )
 
   for file in files:
-    full_name, extension = os.path.splitext(file)
-    new_name = f'{full_name} (Large){extension}'
+    name, extension = os.path.splitext(file)
+    new_name = f'{name} (Large){extension}'
     if extension.lower() == '.jpg':
       if int(os.path.getsize(f'{path}/{file}')) > 700000 and file != 'Thumbs.db':
 
         #resizing images
-        image_width = 1440
-        image_height = 1080
-
         with Image.open(os.path.join(path, file)) as image:
+          image_width = image.width
+          image_height = image.height
+
           image = image.resize((image_width, image_height), PIL.Image.NEAREST)
           image.save(os.path.join(path, new_name))
           shutil.copystat(os.path.join(path, file), os.path.join(path, new_name))
 
+        #removing files
+        if re.search('(Large)', name):
+          os.remove(os.path.join(path, file))
+          print(f'File {file} removed')
+          count += 1
+
         #renaming files
-        if len(full_name.split(' ')) == 2:
-          first_name, second_name = full_name.split(' ')
+        if re.search('(Large)', new_name):
+          first_name, large = name.split(' (Large)')
           first_name = first_name.strip()
           os.rename(os.path.join(path, file), os.path.join(path, f'{first_name}{extension}'))
           count += 1
