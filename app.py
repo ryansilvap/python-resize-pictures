0165b62e4c09966646aa101675f16ade8925e063
Ryan Pereira <ryan.pereira.s10@gmail.com>
2025-03-26 10:55:27 -0300

    Criando o arquivo app.py com a lógica de execução do script e chamada das funções

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from ResizePicture import resize_picture
+from Remove import remove_original_file
+from Rename import rename_resized_file
+
+folder = r'C:\Users\ryan.pereira\Desktop\Imagens'
+count = 0
+
+# 🔥 First loop: Resize images and remove originals
+for path, directories, files in os.walk(folder):
+    for file in files:
+        name, extension = os.path.splitext(file)
+        new_name = f'{name} (Large){extension}'
+        file_path = os.path.join(path, file)
+
+        if extension.lower() == '.jpg' or extension.lower() == '.jpeg' and file != 'Thumbs.db':
+            if os.path.exists(file_path) and os.path.getsize(file_path) > 2000 * 1024:
+                resize_picture(path, file, new_name)
+                remove_original_file(path, file)
+                count += 1
+
+# 🔥 Second loop: Rename resized files back to original name
+for path, directories, files in os.walk(folder):
+    for file in files:
+        name, extension = os.path.splitext(file)
+        if "(Large)" in name:
+            rename_resized_file(path, file, name, extension)
+
+print(f'\nItems processed: {count}')
