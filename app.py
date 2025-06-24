9b05661f96a487a98d07ffddf929616f700a16e8
Ryan Pereira <ryan.pereira.s10@gmail.com>
2025-06-24 18:14:09 -0300

    Primeira parte da funcionalidade de gerar relatório em PDF das imagens que foram compactadas

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+# from fpdf import FPDF
 from ResizePicture import resize_picture
 from Remove import remove_original_file
 from Rename import rename_resized_file
@@ -6,20 +7,29 @@ from Rename import rename_resized_file
 folder = r'C:\Users\ryan.pereira\Desktop\Imagens'
 count = 0
 
-# 🔥 First loop: Resize images and remove originals
+# First loop: Resize images and remove originals
 for path, directories, files in os.walk(folder):
-    for file in files:
-        name, extension = os.path.splitext(file)
-        new_name = f'{name} (Large){extension}'
-        file_path = os.path.join(path, file)
+    if files:
+        # pdf = FPDF()
+        # pdf.add_page()
+        # pdf.set_font('Arial', 'B', 12)
+        # pdf.cell(40, 10, f'{path}', 0)
+        # pdf.ln(10)
 
-        if extension.lower() == '.jpg' or extension.lower() == '.jpeg' and file != 'Thumbs.db':
-            if os.path.exists(file_path) and os.path.getsize(file_path) > 2000 * 1024:
-                resize_picture(path, file, new_name)
-                remove_original_file(path, file)
-                count += 1
+        for file in files:
+            name, extension = os.path.splitext(file)
+            new_name = f'{name} (Large){extension}'
+            file_path = os.path.join(path, file)
 
-# 🔥 Second loop: Rename resized files back to original name
+            if extension.lower() == '.jpg' or extension.lower() == '.jpeg' and file != 'Thumbs.db':
+                if os.path.exists(file_path) and os.path.getsize(file_path) > 2000 * 1024:
+                    resize_picture(path, file, new_name)
+                    remove_original_file(path, file)
+                    count += 1
+
+       # pdf.output(os.path.join(path,'report.pdf'))
+
+# Second loop: Rename resized files back to original name
 for path, directories, files in os.walk(folder):
     for file in files:
         name, extension = os.path.splitext(file)
