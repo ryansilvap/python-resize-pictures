b106ec4db3fc571bac2a076a9a3812547b4e4248
Ryan Pereira <ryan.pereira.s10@gmail.com>
2025-10-02 18:24:31 -0300

    Deletando arquivos desnecessários

--- Remove.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def remove_original_file (path, file):
-  """Remove the original file after resizing."""
-  file_path = os.path.join(path, file)
-
-  if os.path.exists(file_path):
-    os.remove(file_path)
-    print(f'Removed original: {file}')
